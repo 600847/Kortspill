14f748b168f66d30159208a48f131c341bbc847b
600847 <august.helle1@gmail.com>
2021-10-06 15:02:51 +0200

    Ingen ting

--- src/no/hvl/dat100/prosjekt/TODO.java
+++ src/no/hvl/dat100/prosjekt/TODO.java
@@ -1,22 +1,19 @@
 package no.hvl.dat100.prosjekt;
 
 public class TODO {
-//heiigjen
+
 	public static String method() {
-		//haha
-		String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
-		//jweifuhseiuf
-		return "Metoden " + methodName + " er ikke implementert";
-	} //halla gutta boys
 	
-	//sdas
+		String methodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+		
+		return "Metoden " + methodName + " er ikke implementert";
+	} 
+	
+	
 	public static String constructor(String className) {
-				//aha
+				
 	   return "Konstruktøren for klassen " + className + " er ikke implementert";
 		
 	}
 
 }
-//Azzat var her 1
-//jskdjfdskl
-//jhghdydvfgas9 ashdio1
